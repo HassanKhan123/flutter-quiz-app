c0989e4f5f12e6ed26ea4487601dabd27f80805d
HassanKhan123 <hk2602809@gmail.com>
2023-01-22 19:06:18 +0500

    added: scafold widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,28 @@ import "package:flutter/material.dart";
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: const Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: const [
+            Text('The Question!'),
+            ElevatedButton(onPressed: null, child: Text('Answer 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
