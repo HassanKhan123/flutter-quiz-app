6a9e15a48da8ee4eb484ad830f32ff9cabf5dd98
HassanKhan123 <hk2602809@gmail.com>
2023-01-28 17:33:23 +0500

    added: stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestions() {
-    print('Answer called');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
   }
 
   @override
@@ -23,15 +34,11 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            const Text('The Question!'),
+            Text(questions[questionIndex]),
             ElevatedButton(
-                onPressed: () => print('sdsdsd'),
-                child: const Text('Answer 1')),
+                onPressed: answerQuestions, child: const Text('Answer 1')),
             ElevatedButton(
-                onPressed: () {
-                  print('aaa');
-                },
-                child: const Text('Answer 2')),
+                onPressed: answerQuestions, child: const Text('Answer 2')),
             ElevatedButton(
                 onPressed: answerQuestions, child: const Text('Answer 3')),
           ],
