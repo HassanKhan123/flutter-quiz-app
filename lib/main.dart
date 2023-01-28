b9f0ac2e2f0f6d8965d2546744b3e9e6914c0405
HassanKhan123 <hk2602809@gmail.com>
2023-01-28 17:53:09 +0500

    added: private properties and methods

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,16 @@ class MyApp extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestions() {
+  void _answerQuestions() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
   }
 
@@ -34,13 +34,13 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Text(questions[_questionIndex]),
             ElevatedButton(
-                onPressed: answerQuestions, child: const Text('Answer 1')),
+                onPressed: _answerQuestions, child: const Text('Answer 1')),
             ElevatedButton(
-                onPressed: answerQuestions, child: const Text('Answer 2')),
+                onPressed: _answerQuestions, child: const Text('Answer 2')),
             ElevatedButton(
-                onPressed: answerQuestions, child: const Text('Answer 3')),
+                onPressed: _answerQuestions, child: const Text('Answer 3')),
           ],
         ),
       ),
