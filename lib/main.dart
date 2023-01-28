c6c98a1b42493d5617afa4e0ac7e34c68326261d
HassanKhan123 <hk2602809@gmail.com>
2023-01-28 19:14:41 +0500

    added: custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(MyApp());
+import './question.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             ElevatedButton(
                 onPressed: _answerQuestions, child: const Text('Answer 1')),
             ElevatedButton(
