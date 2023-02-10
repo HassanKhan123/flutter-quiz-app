09e2f46ac06ec1e13c3a0a68a52dc96c09acc710
HassanKhan123 <hk2602809@gmail.com>
2023-02-10 16:40:43 +0500

    added: terninary operator

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Zebra', 'Lion', 'Elephant', 'Tiger']
+    }
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestions() {
@@ -25,30 +36,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Zebra', 'Lion', 'Elephant', 'Tiger']
-      }
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]),
-            Answer('Answer1', _answerQuestions),
-            Answer('Answer2', _answerQuestions),
-            Answer('Answer3', _answerQuestions),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText'] as String),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(answer, _answerQuestions);
+                  }).toList()
+                ],
+              )
+            : const Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
