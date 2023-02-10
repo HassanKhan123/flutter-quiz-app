a22c583b8a1c373e3a9da27703a11dd8bf130c56
HassanKhan123 <hk2602809@gmail.com>
2023-02-10 17:47:33 +0500

    code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
-import './question.dart';
-import './answer.dart';
+import './result.dart';
+import './quiz.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static const questions = [
+  static const _questions = [
     {
       'questionText': 'What\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White']
@@ -38,23 +38,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First App'),
-        ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(questions[_questionIndex]['questionText'] as String),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(answer, _answerQuestions);
-                  }).toList()
-                ],
-              )
-            : const Center(
-                child: Text('You did it!'),
-              ),
-      ),
+          appBar: AppBar(
+            title: const Text('My First App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answeruestion: _answerQuestions,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : const Result()),
     );
   }
 }
